c7574bff35a759110e2be56c73a584065eecb11c
Aryan Martins <aryan@r10consultoria.com.br>
2025-05-27 17:17:19 -0300

    Atualização das configurações do banco de dados para desenvolvimento local

--- teamflow/settings.py
+++ teamflow/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,aryanmartins.pythonanywhere.com').split(',')
 
 
 # Application definition
@@ -83,6 +83,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    } if DEBUG else {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aryanmartins$gerenciador',
+        'USER': 'aryanmartins',
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': 'aryanmartins.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
